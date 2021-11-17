05405ffa2ec8be57b8878ebb0393b9322a2e3479
Vlad <koreckaya10@gmail.com>
2021-11-17 17:39:42 +0200

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,7 @@ import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div className="header"></div>
-    <div className="content">
-      <div className="content__options">
-        <div className="content__options-button"></div>
-        <div className="content__options-filter"></div>
-      </div>
-      <div className="content__items-information"></div>
-    </div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
