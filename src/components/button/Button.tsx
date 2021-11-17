42aeccb04bddf8d4b2748a8b61dd05a9d9e2f51a
created4GitHub <90993771+created4GitHub@users.noreply.github.com>
2021-11-17 16:31:29 +0200

    Update Button.tsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,1 +1,22 @@
-s
+import React, {MouseEventHandler} from "react";
+
+import './Button.style.css';
+
+type Props = {
+  className: string,
+  disabled : boolean | undefined,
+  btnText: string
+  onClick: void | MouseEventHandler<HTMLButtonElement> | undefined,
+};
+
+export const Button : React.FC<Props> = (props) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className={props.className}
+      disabled={props.disabled}
+    >
+      {props.btnText}
+    </button>
+  )
+}
