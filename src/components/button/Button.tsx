46b1144dc6fdd05ade4898606ea27d5e5b7dceaf
created4GitHub <90993771+created4GitHub@users.noreply.github.com>
2021-11-17 16:31:21 +0200

    Update Button.tsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,1 @@
-import React from "react";
-
-import './Button.style.css';
-
-export const Button : React.FC = (props) => {
-  return (
-    <button
-      onClick={props.onClick}
-      className={props.className}
-      disabled={props.disabled}
-    >
-      {props.btnText}
-    </button>
-  )
-}
+s
