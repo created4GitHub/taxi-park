9e69b540df12c86e41a144866f60b5d8ab73c7da
Yaroslav Resnenko <rocker15646@gmail.com>
2021-11-23 11:22:27 +0200

    fuf

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React, { useEffect, useState, useRef, useContext } from "react";
+=======
+import React, { useContext } from "react";
+>>>>>>> parent of 4a015cc (merge)
 import { Context } from "../../context";
 
 import AddForm from '../addDrivers/addForm'
@@ -6,19 +10,25 @@ import FormSection from "../formSection/FormSection";
 
 import "./form.scss";
 
+<<<<<<< HEAD
 const Form = (props: any) => {
+=======
+const Form = (props:any) => {
+>>>>>>> parent of 4a015cc (merge)
   const [context, setContext] = useContext(Context);
 
+  console.log(props);
+
   return (
-        <div className="table">
-          {context ? <AddForm /> : ''}
-            {props.info.map((item : any, index : any) => {
-              return (
-                <FormSection key={index} {...{ info: item, statuses: props.statuses }} />
-              );
-            })}
-        </div>
-      ) 
-        }
+    <div className="table">
+      { context ? <AddForm /> : '' }
+      { props === undefined ? props.info.map((item : any, index : any) => {
+        return (
+          <FormSection key={ index } {...{ info: item, statuses: props.statuses }} />
+        );
+      }) : '' }
+    </div>
+  ) 
+}
 
 export default Form;
