4a015ccbd0a95177a74236c5215793958908cdd7
Yaroslav Resnenko <rocker15646@gmail.com>
2021-11-23 11:06:22 +0200

    merge

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,31 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
+// import { Context } from "../../context";
+
+// import AddForm from '../addformComponent/addForm'
+// import FormSection from "../formSection/FormSection";
+
+// import "./form.scss";
+
+// const Form = (props:any) => {
+//   const [context, setContext] = useContext(Context);
+
+//   console.log(props);
+
+//   return (
+//     <div className="table">
+//       { context ? <AddForm /> : '' }
+//       { props === undefined ? props.info.map((item : any, index : any) => {
+//         return (
+//           <FormSection key={ index } {...{ info: item, statuses: props.statuses }} />
+//         );
+//       }) : '' }
+//     </div>
+//   ) 
+// }
+
+// export default Form;
+
+import React, { useEffect, useState, useRef, useContext } from "react";
 import { Context } from "../../context";
 
 import AddForm from '../addformComponent/addForm'
@@ -6,21 +33,22 @@ import FormSection from "../formSection/FormSection";
 
 import "./form.scss";
 
-const Form = (props:any) => {
+const Form = (props: any) => {
+  const statuses = useRef([]);
+  const info = useRef([]);
+  const [isReceived, setIsReceived] = useState(false);
   const [context, setContext] = useContext(Context);
 
-  console.log(props);
-
   return (
-    <div className="table">
-      { context ? <AddForm /> : '' }
-      { props === undefined ? props.info.map((item : any, index : any) => {
-        return (
-          <FormSection key={ index } {...{ info: item, statuses: props.statuses }} />
-        );
-      }) : '' }
-    </div>
-  ) 
-}
+        <div className="table">
+          {context ? <AddForm /> : ''}
+            {props.info.map((item : any, index : any) => {
+              return (
+                <FormSection key={index} {...{ info: item, statuses: props.statuses }} />
+              );
+            })}
+        </div>
+      ) 
+        }
 
 export default Form;
