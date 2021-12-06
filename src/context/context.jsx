39e8f85e281423817df55950a96174cabd9c05ae
Vlad <koreckaya10@gmail.com>
2021-12-06 14:00:34 +0200

    latest

--- src/context/context.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const receivedDataContext = React.createContext();
-
-export const filteredDataContext = React.createContext();
-
-export const filteredValuesContext = React.createContext();
