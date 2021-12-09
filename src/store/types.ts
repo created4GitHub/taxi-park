bb21a0e6817fef7b779c07de15e1766042feef5e
Dima-JC <70806698+Dima-JC@users.noreply.github.com>
2021-12-09 10:06:23 +0200

    ..

--- src/store/types.ts
+++ src/store/types.ts
@@ -3,5 +3,3 @@ export const FILTER_DATA = "FILTER_DATA";
 export const RESET_FILTER = "RESET_FILTER";
 export const SET_IS_OPEN = 'SET_IS_OPEN';
 export const IS_DATA_UPDATED = 'IS_DATA_UPDATED';
-
-
