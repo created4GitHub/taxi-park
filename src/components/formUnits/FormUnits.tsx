d1e1d98e76a29dc1b1e73dfde6fbea4a9175e044
Vlad <koreckaya10@gmail.com>
2021-12-10 10:20:52 +0200

    meta

---
+++ src/components/formUnits/FormUnits.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Loader from "../loader/loader";
+import FormSection from "./formSection/FormSection";
+import UnitsTitles from "./unitsTitles/UnitsTitles";
+import { dispatchData } from "../../store/actions/actions";
+import { GET, GET_STATUS } from "../../requests/requests";
+import { RootState } from "../../store/rootReducer";
+import { Data } from "../../interfaces/interfaces";
+
+import "./formUnits.style.scss";
+const selector = (state: RootState) => state.filteredData
+interface Props {
+  title: string;
+}
+
+const FormUnits = ({ title }: Props) => {
+  const [isDeleted, setIsDeleted] = useState<boolean>(true);
+  const receivedData = useSelector((state: RootState) => state.data);
+  const filteredData = useSelector(selector);
+  const isFilteredData = useSelector((state: RootState) => state.isDataFiltered);
+  const isDataUpdated = useSelector((state: RootState) => state.isDataUpdated);
+  const isAddNewUnit = useSelector((state: RootState) => state.isAddNewUnit);
+  const data = (isFilteredData && filteredData) || receivedData;
+  const dispatch = useDispatch();
+
+  async function fetchData() {
+    const data = await GET(title) as Data[];
+    const statuses = await GET_STATUS(title);
+    dispatch(dispatchData({ data, statuses }));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [isDeleted, title, isDataUpdated, isAddNewUnit]);
+
+  const mapItems = (item: Data) => {
+    return (
+      <FormSection
+        key={item.id}
+        title={title}
+        data={item}
+        setIsDeleted={setIsDeleted}
+      />
+    );
+  }
+
+  const mappedItems = data.map(mapItems)
+
+  return (
+    <>
+      <UnitsTitles title={title} />
+      {data.length ?
+        mappedItems
+        : <>{!isFilteredData && <div><Loader /></div>}</>
+      }
+    </>
+  );
+};
+
+export default FormUnits;
---
+++ src/components/formUnits/formSection/formSectionTab/FormSectionTab.tsx
@@ -0,0 +1,94 @@
+import { useState, MouseEvent } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Statuses from "../../../statuses/Statuses";
+import { PATCH } from "../../../../requests/requests";
+import { RootState } from "../../../../store/rootReducer";
+import { Data, Status } from "../../../../interfaces/interfaces";
+import { dispatchIsDataUpdated } from "../../../../store/actions/actions";
+
+import "./formSectionTab.style.scss";
+
+interface Props {
+  value: string | number | Status;
+  property: string;
+  title: string;
+  id: string;
+  data: Data;
+}
+
+const FormSectionTab = ({ value, property, title, data, id }: Props) => {
+  const statuses = useSelector((state: RootState) => state.statuses);
+  const [selectValue, setSelectValue] = useState<string>((value as Status).title);
+  const [isDiv, setIsDiv] = useState<boolean>(true);
+  const dispatch = useDispatch();
+
+  const updateElementType = (event: MouseEvent<HTMLElement>) => {
+    if (!["id", "date_birth", "date_created", "driver_id"].includes(property)) {
+      if (!((event.target as HTMLElement).className === "table_input")) {
+        setIsDiv(!isDiv);
+      }
+    }
+  }
+
+  const saveNewInformation = (newValue: string | Status) => {
+    (data[property as keyof Data] as string | Status) = newValue;
+    setIsDiv(!isDiv);
+    PATCH(title, id, { [property]: newValue });
+    dispatch(dispatchIsDataUpdated());
+  };
+
+  const pressedEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      const target = event.target as HTMLInputElement;
+      target.value ? saveNewInformation(target.value) : setIsDiv(!isDiv);
+    }
+  };
+
+  const onBlurEvent = (event: React.FocusEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    target.value ? saveNewInformation(target.value) : setIsDiv(!isDiv);
+  };
+
+  const saveStatus = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newTitle = event.target.value;
+    const status = statuses.find((status: Status) => status.title === newTitle)!;
+    setSelectValue(newTitle);
+    saveNewInformation(status);
+  };
+
+  return (
+    <>
+      <div className="table-section-tab">
+        {property !== "status" ? (
+          isDiv ? (
+            <p className="table_paragraph" onClick={updateElementType} >
+              {data[property as keyof Data]}
+            </p>
+          ) : (
+            <input
+              type="text"
+              placeholder={String(data[property as keyof Data])}
+              className="table_input"
+              autoFocus={true}
+              onClick={updateElementType}
+              onBlur={onBlurEvent}
+              onKeyPress={pressedEnter}
+            />)
+        ) : (
+          <select
+            name="status"
+            className="table_section-tab-select"
+            value={selectValue}
+            onChange={saveStatus}
+            id={property}
+          >
+            <Statuses />
+          </select>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default FormSectionTab;
